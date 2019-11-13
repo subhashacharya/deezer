67a012fe76acdeaa91c6579a9af48d174fadc8c8
Subhash <subhash2046@gmail.com>
2019-11-13 15:09:46 +0545

    Improve keypad ime action event
    * Use "actionSearch" as imeoption
    * Dismiss keypad on keypad's search icon click

--- app/src/main/java/com/subhash/deezer/di/AppModule.kt
+++ app/src/main/java/com/subhash/deezer/di/AppModule.kt
@@ -1,9 +1,12 @@
 package com.subhash.deezer.di
 
 import android.content.Context
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat
 import com.subhash.deezer.DeezerApplication
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class AppModule {
@@ -11,5 +14,11 @@ class AppModule {
     @Provides
     fun provideContext(application: DeezerApplication): Context {
         return application.applicationContext
+    }
+
+    @Provides
+    @Singleton
+    fun providesInputMethodManager(context: Context): InputMethodManager {
+        return requireNotNull(ContextCompat.getSystemService(context, InputMethodManager::class.java))
     }
 }
--- app/src/main/java/com/subhash/deezer/ui/search/SearchFragment.kt
+++ app/src/main/java/com/subhash/deezer/ui/search/SearchFragment.kt
@@ -4,11 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo.IME_ACTION_SEARCH
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import com.subhash.deezer.R
 import com.subhash.deezer.databinding.SearchFragmentBinding
 import com.subhash.deezer.ui.utils.getViewModel
 import dagger.android.support.DaggerFragment
@@ -18,6 +19,8 @@ class SearchFragment : DaggerFragment() {
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
+    @Inject
+    lateinit var inputMethodManager: InputMethodManager
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,6 +37,14 @@ class SearchFragment : DaggerFragment() {
             searchResults.adapter = SearchAdapter(
                 searchViewModel.searchViewState, searchViewModel, viewLifecycleOwner
             )
+
+            searchEditText.setOnEditorActionListener { v, actionId, _ ->
+                if (actionId == IME_ACTION_SEARCH) {
+                    //Search is handled on each text change, just close the keypad
+                    inputMethodManager.hideSoftInputFromWindow(v.windowToken, 0);
+                }
+                false
+            }
 
             viewModel = searchViewModel
             listener = searchViewModel
